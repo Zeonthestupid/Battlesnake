311a297b65b8ec8e0d85b6236dca195569979679
Micah S <145581856+Zeonthestupid@users.noreply.github.com>
2024-04-28 18:53:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -98,16 +98,17 @@ def snakematrix(matrix, game_state, snake_weight):
 def foodmatrix(matrix, game_state, snake_weight):
     snakeid = game_state["you"]["id"]
     snake_weight = snake_weight * 1
+     
     for snake in game_state["board"]["snakes"]:
         if snake['id'] == snakeid:
           snakelength = len(snake['body'])
-    for snake in game_state["board"]["snakes"]:
+    for food in game_state["board"]["food"]:
+    
         if snake['id'] != snakeid:
           if len(snake["body"]) < snakelength: # if their snake is smaller than my snake
-              decaytiles(body["x"], body["y"], -10, matrix, game_state)
+              decaytiles(food["x"], food["y"], -10, matrix, game_state)
           else:
-              decaytiles(body["x"], body["y"], snake_weight, matrix, game_state)
-
+              decaytiles(food["x"], food["y"], snake_weight, matrix, game_state)
 
 def move(game_state: typing.Dict) -> typing.Dict:
     snakeid = game_state["you"]["id"]
